6c1c198cf525ce143156a81b56ccaf7b3cf61a6c
Шарафутдинов Ильгиз <ilgiz93sharafutdinov@gmail.com>
2024-02-29 16:04:35 +0500

    third

--- inversions.js
+++ inversions.js
@@ -1,31 +1,47 @@
-function inversions(arr0) {
-    let mid = arr0.length / 2
-    if (arr0.length < 2) {
-        return { arr: arr0, inv: 0 }
-    }
-    let { arr: arr1, inv: leftInv } = invertions(arr0.slice(0, mid))
-    let { arr: arr2, inv: rightInv } = invertions(arr0.slice(mid))
+/**
+ * Merge two arrays and count the number of inversions.
+ *
+ * @param {Array} A - the first input array
+ * @param {Array} B - the second input array
+ * @return {Object} an object containing the merged array and the number of inversions
+ */
+const merge = (A, B) => {
+    let inv = A.inv + B.inv
 
-    return merge({ arr: arr1, inv: leftInv }, { arr: arr2, inv: rightInv })
-}
-
-
-function merge(arr1, arr2) {
-    let inv = arr1.inv + arr2.inv
-    let result = []
-    while (arr1.arr.length && arr2.arr.length) {
-        if (arr1.arr[0] < arr2.arr[0]) {
-            result.push(arr1.arr[0])
-            arr1.arr.shift()
+    let C = []
+    let i = 0
+    let j = 0
+    while (i < A.arr.length && j < B.arr.length) {
+        if (A.arr[i] < B.arr[j]) {
+            C.push(A.arr[i])
+            i++
         } else {
-            result.push(arr2.arr[0])
-            inv = inv + 1
-            arr2.arr.shift()
+            C.push(B.arr[j])
+            j++
+            inv++
         }
     }
-    return { arr: [...result, ...arr1.arr, ...arr2.arr], inv }
+    if (i < A.arr.length) {
+        C = C.concat(A.arr.slice(i))
+    }
+    if (j < B.arr.length) {
+        C = C.concat(B.arr.slice(j))
+    }
+    return { arr: C, inv }
 }
-
+//[1, 3, 5], [2, 4, 6]
+const invertions = (arr) => {
+    let mid = arr.length / 2
+    if (arr.length < 2) {
+        return { arr, inv: 0 }
+    }
+    let { arr: arr1, inv: leftInv } = invertions(arr.slice(0, mid))
+    let { arr: arr2, inv: rightInv } = invertions(arr.slice(mid))
+    return merge({ arr: arr1, inv: leftInv }, { arr: arr2, inv: rightInv })
+}
+//[1,2,3]
+//[2,3,1]
+//[1]
 
 const p = {
     niyaz: [
@@ -98,10 +114,10 @@ function init(targetP, comparedP) {
         el ? result.push(el.rate) : null
     }
     console.log(result)
-    const inv = inversions(result)
+    const inv = invertions(result)
     return inv
 }
 
-const inv = init(p.pavel, p.fedor)
+const inv = init(p.pavel, p.uraD)
 console.log(inv)
 console.log((1 - inv.inv/p.alexey.length) * 100 )
